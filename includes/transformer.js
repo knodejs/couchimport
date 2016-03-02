2c58d6956708235b7fb2c022d210027b99eaed99
Narongchai Thongkongkeaw <knodejs@gmail.com>
2016-03-02 12:08:24 +0700

    update lowercase property

--- includes/transformer.js
+++ includes/transformer.js
@@ -1,18 +1,29 @@
 var stream = require('stream');
-
 module.exports = function(func, meta) {
-  
-  var transformer = new stream.Transform( { objectMode: true } );
-  transformer._transform = function (obj, encoding, done) {
-    // transform using custom function
-    if(typeof func == "function") {
-      obj = func(obj, meta);
-    }
 
-    // pass object to next stream handler
-    this.push( obj);
-    done();
-  };
-  
-  return transformer;
+    var transformer = new stream.Transform({ objectMode: true });
+    transformer._transform = function(obj, encoding, done) {
+        // transform using custom function
+        if (typeof func == "function") {
+            obj = func(obj, meta);
+        }
+        var newObj=lowerObject(obj);
+        //console.log(newObj);
+        // pass object to next stream handler
+        this.push(newObj);
+        done();
+    };
+
+    return transformer;
+}
+
+function lowerObject(obj) {
+    var key, keys = Object.keys(obj);
+    var n = keys.length;
+    var newobj = {}
+    while (n--) {
+        key = keys[n];
+        newobj[key.toLowerCase()] = obj[key];
+    }
+    return newobj;
 }
